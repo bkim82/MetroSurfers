7d7002c2f7e5e8d13b1c0244b3002786142e279f
liam2smith <liam2004smith@gmail.com>
2026-02-16 17:11:00 -0500

    Added coin collection and UI for coin collections

--- Assets/Scripts/PlayerHit.cs
+++ Assets/Scripts/PlayerHit.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerHit : MonoBehaviour
 {
+
+    public TextMeshProUGUI scoreText;
+
+    private int score;
+
+    void Start()
+    {
+        SetScore();
+    }
+    
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.collider.GetComponent<Obstacle>())
@@ -9,5 +20,18 @@ public class PlayerHit : MonoBehaviour
             Debug.Log("HIT - GAME OVER");
             Time.timeScale = 0f;
         }
+        if (hit.collider.CompareTag("Coin"))
+        {
+            hit.collider.gameObject.GetComponent<Collider>().enabled = false;
+            Destroy(hit.collider.gameObject);
+            score++;
+            SetScore();
+        }
+
+    }
+
+    void SetScore()
+    {
+        scoreText.text = "Score: " + score.ToString();
     }
 }
